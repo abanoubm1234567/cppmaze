d937b1d1b140198e277f243c40524a7cb7254ea9
abanoub_m <127553246+abanoubm1234567@users.noreply.github.com>
2025-10-24 12:04:37 -0400

    Add files via upload

--- maze.cpp
+++ maze.cpp
@@ -1,6 +1,8 @@
+#include <chrono>
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
+#include <thread>
 
 void initMaze(char* p, int size);
 void printMaze(char* p, int size);
@@ -14,11 +16,11 @@ char* get_up(char* p, int size);
 char* get_down(char* p, int size);
 
 int main(){
-    unsigned int size = 10; // maze will be 40x40 
+    unsigned int size = 50; // maze will be 40x40 
     char maze[size][size];
     srand(time(0));
     initMaze((char*)maze, size );
-    printMaze((char*)maze, size);
+    //printMaze((char*)maze, size);
     generateMaze((char*)maze, size);
     printMaze((char*) maze, size);
     return 0;
@@ -43,6 +45,8 @@ void initMaze(char* p, int size){
 }
 
 void printMaze(char *p, int size){
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    std::system("clear");
     std::cout<<std::endl;
     for (int i = 0; i<size*size; i++) {
         std::cout<<*(p+i);
@@ -70,68 +74,71 @@ void backtracking(char* start, char *p, int size){
         return;
     }
     char options[4] = {'l','r','d','u'}; //representing four directions
-    char choice = options[rand()%4];
     *p = ' ';
     bool l_avail = *(get_left(p, size))=='.';
     bool r_avail = *(get_right(p, size))=='.';
     bool d_avail = *(get_down(p, size))=='.';
     bool u_avail = *(get_up(p, size))=='.';
-    std::cout<<"x: "<<get_x_pos(start, p, size)<<"\ty: "<<get_y_pos(start, p, size)<<std::endl;
+    //std::cout<<"x: "<<get_x_pos(start, p, size)<<"\ty: "<<get_y_pos(start, p, size)<<std::endl;
     printMaze(start, size);
     while( l_avail || r_avail || d_avail || u_avail ){
-    switch (choice) {
-        case 'l':
-            if (l_avail){
-                l_avail = false;
-                if (*(p-size-1) != ' '){
-                    (*(p-size-1) = 'X');
+        char choice = options[rand()%4];
+        switch (choice) {
+            case 'l':
+                if (l_avail){
+                    //std::cout<<"Going left"<<std::endl;
+                    l_avail = false;
+                    if (*(get_up(p, size)) != ' '){
+                        (*(get_up(p, size)) = 'X');
+                    }
+                    if (*(get_down(p, size)) != ' '){
+                        (*(get_down(p, size)) = 'X');
+                    }
+                    backtracking(start, p-1, size);
+                    break;
                 }
-                if (*(p+size-1) != ' '){
-                    (*(p+size-1) = 'X');
+            case 'r':
+                if (r_avail){
+                    //std::cout<<"Going right"<<std::endl;
+                    r_avail = false;
+                    if (*(get_up(p, size)) != ' '){
+                        (*(get_up(p, size)) = 'X');
+                    }
+                    if (*(get_down(p, size)) != ' '){
+                        (*(get_down(p, size)) = 'X');
+                    }
+                    backtracking(start, p+1, size);
+                    break;
                 }
-                backtracking(start, p-1, size);
-                break;
-            }
-        case 'r':
-            if (r_avail){
-                r_avail = false;
-                if (*(p-size+1) != ' '){
-                    (*(p-size+1) = 'X');
+            case 'd':
+                if (d_avail){
+                    //std::cout<<"Going down"<<std::endl;
+                    d_avail = false;
+                    if (*(get_left(p, size)) != ' '){
+                        (*(get_left(p, size)) = 'X');
+                    }
+                    if (*(get_right(p, size)) != ' '){
+                        (*(get_right(p, size)) = 'X');
+                    }
+                    backtracking(start, p+size, size);
+                    break;
                 }
-                if (*(p+size+1) != ' '){
-                    (*(p+size+1) = 'X');
+            case 'u':
+                if (u_avail){
+                    //std::cout<<"Going up"<<std::endl;
+                    u_avail = false;
+                    if (*(get_left(p, size)) != ' '){
+                        (*(get_left(p, size)) = 'X');
+                    }
+                    if (*(get_right(p, size)) != ' '){
+                        (*(get_right(p, size)) = 'X');
+                    }
+                    backtracking(start, p-size, size);
+                    break;
                 }
-                backtracking(start, p+1, size);
-                break;
-            }
-        case 'd':
-            if (d_avail){
-                d_avail = false;
-                if (*(p+size-1) != ' '){
-                    (*(p+size-1) = 'X');
-                }
-                if (*(p+size+1) != ' '){
-                    (*(p+size+1) = 'X');
-                }
-                backtracking(start, p+size, size);
-                break;
-            }
-        case 'u':
-            if (u_avail){
-                u_avail = false;
-                if (*(p-size-1) != ' '){
-                    (*(p-size-1) = 'X');
-                }
-                if (*(p-size+1) != ' '){
-                    (*(p-size+1) = 'X');
-                }
-                backtracking(start, p-size, size);
-                break;
-            }
-
+        }
     }
-    
-}
+    return;
 }
 
 int get_x_pos(char* start, char* p, int size){
@@ -141,7 +148,6 @@ int get_x_pos(char* start, char* p, int size){
 int get_y_pos(char* start, char* p, int size){
     return (p-start)/size;
 }
-
 
 char* get_left(char* p, int size){
     return p-1;
